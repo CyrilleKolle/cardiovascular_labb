7211b9714c679f6f66453aa38a6cd9dee2e2bc1c
CyrilleKolle <ngidekollecyrille@gmail.com>
2023-03-23 10:20:11 +0100

    added multiplot for features

--- code/utils.py
+++ code/utils.py
@@ -40,3 +40,41 @@ def pipe_model(scaler, model):
     pipe = Pipeline([scaler, model])
     pipe.fit(explainatory, response)
     return pipe
+
+
+
+def patient_diagnostic_for_cardiovascular_disease(scaler, model):
+    pipe = pipe_model(scaler=scaler, model=model)
+    prompts = ['Enter value for systolic blood pressure (ap_hi): ',
+               'Enter value for diastolic blood presure (ap_lo): ',
+               'Between 1 to 3 enter value for cholesterol: ',
+               'Between 1 to 3 enter value for glucose: ',
+               'Between 0 or 1, enter value for smoker: ',
+               'Between 0 or 1, enter value for alcohol consumption: ',
+               'Between 0 or 1 enter value for active: ',
+               'Enter value for age: ',
+               'Enter value for bmi: ',
+               'Between 1 or 2, enter value for gender: ']
+    
+    values = []
+    for prompt in prompts:
+        value = input(prompt)
+        values.append(value)
+    
+    df = pd.DataFrame({'ap_hi': [values[0]],
+                       'ap_lo': [values[1]],
+                       'cholesterol': [values[2]],
+                       'gluc': [values[3]],
+                       'smoke': [values[4]],
+                       'alco': [values[5]],
+                       'active': [values[6]],
+                       'age_years': [values[7]],
+                       'bmi': [values[8]],
+                       'gender_women': [values[9]]})
+    
+    prediction = pipe.predict(df)
+    
+    if prediction[0] == 0:
+        print(f"The diagnosis suggest you dont have any cardiovascular disease. But if you have any concerns about your health contact a professional.")
+    else:
+        print(f"The diagnosis suggest you MIGHT have a cardiovascular disease. These results are not DEFINITIVE! Contact a professional!!!.")
